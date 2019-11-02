d72d313576924b02dd93c7aae8f57f7e591d0bb5
ayxan386 <ayxanpro@gmail.com>
2019-11-02 18:25:05 +0400

    -- getAllUsers test added

--- src/main/java/User/UserController.java
+++ src/main/java/User/UserController.java
@@ -5,7 +5,14 @@ import java.util.ArrayList;
 public class UserController {
         private UserService userService;
 
+        private UserController() {
+                userService = new UserService();
+        }
         public ArrayList<User> getAllUsers() {
                 return userService.getAllUsers();
+        }
+
+        public static UserController create() {
+                return new UserController();
         }
 }
--- src/test/java/User/UserControllerTest.java
+++ src/test/java/User/UserControllerTest.java
@@ -1,5 +1,25 @@
 package User;
 
-public class UserControllerTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UserControllerTest {
+        private UserController userController;
+
+        @Before
+        public void setup() {
+                userController = UserController.create();
+        }
+
+        @Test
+        public void getAllUsers() {
+                ArrayList<User> allUsers = userController.getAllUsers();
+                ArrayList<User> expected = new ArrayList<User>();
+                assertThat(allUsers, equalTo(expected));
+        }
 }
