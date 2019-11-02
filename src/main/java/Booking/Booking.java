90fb7d5fa7806c6bbe2ede127baddcdb664faff3
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-11-03 00:28:50 +0400

    Added DAO interface with methods,Added fields to Booking and Flight

--- src/main/java/Booking/Booking.java
+++ src/main/java/Booking/Booking.java
@@ -1,4 +1,40 @@
 package Booking;
 
 public class Booking {
+    private int date;
+    private int price;
+    private int serialNumber;
+    private boolean Class;
+
+    public boolean isClass() {
+        return Class;
+    }
+
+    public void setClass(boolean aClass) {
+        Class = aClass;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/main/java/Flight/Flight.java
+++ src/main/java/Flight/Flight.java
@@ -1,4 +1,67 @@
 package Flight;
 
-public class Flight {
+public class Flight{
+    private int flightID;
+    private String flightName;
+    private String source;
+    private String departure;
+    private int arrivalTime;
+    private int departureTime;
+    private int seats;
+
+    public int getFlightID() {
+        return flightID;
+    }
+
+    public void setFlightID(int flightID) {
+        this.flightID = flightID;
+    }
+
+    public String getFlightName() {
+        return flightName;
+    }
+
+    public void setFlightName(String flightName) {
+        this.flightName = flightName;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public int getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(int departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
 }
