0b0887c500d49b865541227f42c8b980c136e805
H-Orkhan <orxan.hashimov2014@gmail.com>
2019-11-08 19:09:24 +0400

    Added CommandList CommandParser and UserInterface(need complete switch case)

--- src/main/java/Booking/BookingController.java
+++ src/main/java/Booking/BookingController.java
@@ -1,25 +1,34 @@
 package Booking;
 
 public class BookingController {
-        private BookingService bookingService;
+    private final BookingService bookingService;
 
-        public static BookingController create() {
-                return new BookingController();
-        }
+    private BookingController() {
+        this.bookingService = new BookingService();
+    }
 
-        public boolean add(Booking b1) {
-                throw new IllegalArgumentException("Not implemented yet");
-        }
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
 
-        public Booking get(String id) {
-                throw new IllegalArgumentException("Not implemented yet");
-        }
+    public Booking get(String id) {
+        return bookingService.get(id);
+    }
 
-        public boolean remove(Booking booking) {
-                throw new IllegalArgumentException("Not implemented yet");
-        }
+    public boolean add(Booking book) {
+        return bookingService.smartAdd(book);
+    }
 
-        public void eraseAll() {
-                throw new IllegalArgumentException("Not implemented yet");
-        }
+    public static BookingController create() {
+        return new BookingController();
+    }
+
+    public void eraseAll() {
+        bookingService.eraseData();
+    }
+
+    public boolean remove(Booking book) {
+        return bookingService.smartRemove(book);
+    }
+
 }
---
+++ src/main/java/Booking/BookingDAO.java
@@ -0,0 +1,82 @@
+package Booking;
+import DAO.DAO;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class BookingDAO implements DAO<Booking> {
+
+    private HashSet<Booking> storage;
+    private final static File bookings = new File("data", "bookings.txt");
+
+    public BookingDAO() {
+        storage = new HashSet<Booking>();
+    }
+
+    public void updateDatabase() {                              //See 1st comment
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(bookings));
+            for (Booking bookings : storage) {
+                bw.write(
+                        String.format("%s/%s/%s/%s/%s\n",
+                                bookings.getFlight(),
+                                bookings.getID(),
+                                bookings.getDate(),
+                                bookings.getPrice(),
+                                bookings.getClass()
+                        ));
+            }
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void add(Booking data) {
+        storage.add(data);
+        updateDatabase();
+    }
+
+    @Override
+    public void remove(String id) {
+        storage.removeIf(el -> el.getID().equals(id));
+        updateDatabase();
+    }
+
+    @Override
+    public Booking get(String id) {
+        return storage.stream()
+                .filter(el -> el.getID().equals(id))
+                .findFirst().get();
+    }
+
+    @Override
+    public void update(Booking data) {
+        storage.add(data);
+    }
+
+
+    public boolean contains(Booking data) {
+        return storage.contains(data);
+    }
+
+    public void eraseData() {
+        try {
+            BufferedWriter bw = new BufferedWriter(
+                    new FileWriter(bookings));
+            bw.write("");
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashSet<Booking> getAllBookingInfo() {
+        return storage;
+    }
+}
